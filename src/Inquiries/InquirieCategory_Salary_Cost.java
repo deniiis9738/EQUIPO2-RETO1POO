fc3f299b716b0062bb23455c5a90608f85e97eff
carloschg1 <aluher5242@ieselcaminas.org>
2023-02-21 12:02:30 +0100

    V6

---
+++ src/Inquiries/InquirieCategory_Salary_Cost.java
@@ -0,0 +1,8 @@
+package Inquiries;
+
+public class InquirieCategory_Salary_Cost extends Inquiries{
+    @Override
+    public void realizeInquirie() {
+
+    }
+}
---
+++ src/Modify/Modify_Add.java
@@ -0,0 +1,13 @@
+package Modify;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public abstract class Modify_Add {
+
+    public abstract void realizeAdd();
+
+}
