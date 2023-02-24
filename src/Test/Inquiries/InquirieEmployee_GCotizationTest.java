c1bddd7a28cce5dbf07786aaaf87932b6d760001
chg1 <aluher5242@ieselcaminas.org>
2023-02-24 20:18:21 +0100

    Reto - V2 POO

--- src/Test/Inquiries/InquirieEmployee_GCotizationTest.java
+++ src/Test/Inquiries/InquirieEmployee_GCotizationTest.java
@@ -3,24 +3,13 @@ package Test.Inquiries;
 import Documents.Read_Empleoyee;
 import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayInputStream;
-import org.junit.jupiter.api.Test;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import Documents.Read_Department;
-import Documents.Read_Empleoyee;
 import Inquiries.InquirieEmployee_GCotization;
-import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
-import java.util.Scanner;
 
-import static org.junit.jupiter.api.Assertions.*;
 
 class InquirieEmployee_GCotizationTest {
     /**
@@ -45,31 +34,6 @@ class InquirieEmployee_GCotizationTest {
         }
         inquirieEmployeeGCotization.realizeInquirie();
 
-    }
-    public static Scanner setinputValue = new Scanner(System.in);
-
-    @Test
-    public void testRealizeInquirie() {
-        // Setup
-        InquirieEmployee_GCotization inquiry = new InquirieEmployee_GCotization();
-        //inquiry.setInputValue(new Scanner("2\n")); // Simula la entrada del usuario como si hubiera ingresado "2"
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        System.setOut(new PrintStream(outputStream)); // Redirecciona la salida de la consola a outputStream
-
-        // Ejecución
-        inquiry.realizeInquirie();
-        String actualOutput = outputStream.toString();
-
-        // Verificación
-        String expectedOutput = "Introduce grupo de cotización: \n" +
-                "NIF            Nombre         Apellido1      Apellido2      Cuenta         Antigüedad     N. SS        " +
-                "Categoria G.Profesional   G.Cotización  Id Departamento\n" +
-                "------------------------------------------------------------------------------------------" +
-                "-----------------------------------------------------------------------------------------------------\n" +
-                "12345678A      Pepe           Pérez          Pérez          ES0123456789   5 años         11111111111   " +
-                "Profesor                  2             1\n" +
-                "\n";
-        assertEquals(expectedOutput, actualOutput);
     }
 
 
